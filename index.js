c883bf9a470181971f87592b2b8ac79d79a4b214
lanakimova <52796981+lanakimova@users.noreply.github.com>
2021-01-05 18:40:06 -0800

    add textfor each page, icons for dropdown button

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const template = document.createElement('template');
 template.innerHTML = 
     `<div class='mt-auto bg-light'>
         <footer>
-            <ul class='list-inline'>
-                <li class="list-inline-item"><a href="#">OpenWeather</a></li>
-                <li class="list-inline-item"><a href="#">Bootstrap</a></li>
-            </ul>
             <p class="copyright">EarthScience © 2021</p>
         </footer>
 
